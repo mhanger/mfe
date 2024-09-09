3f0c2b1bfb355bf0719274f5f6a03494e4accbe5
Marc Hanger <mhangeril@gmail.com>
2024-09-09 14:36:09 -0500

    add header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {BrowserRouter} from 'react-router-dom';
+import { StylesProvider, createGenerateClassName} from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
-  return <div>
-  Container!
-  <hr />
-  <MarketingApp />
-  </div>
+  return (
+
+      <BrowserRouter>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </StylesProvider>
+      </BrowserRouter>
+
+  )
 };
