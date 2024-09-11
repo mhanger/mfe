a885378b56513113c7051d4a34728efbbec133c1
Marc Hanger <mhangeril@gmail.com>
2024-09-11 14:49:17 -0500

    fix webpack prod for container app

--- packages/container/src/components/Progress.js
+++ packages/container/src/components/Progress.js
@@ -1,7 +1,5 @@
 import React from "react";
-
 import { makeStyles, createStyles } from "@material-ui/core/styles";
-
 import LinearProgress from '@material-ui/core/LinearProgress';
 
 const useStyles = makeStyles((theme) => {
